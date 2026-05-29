6ed00d89cc82b3e3e7a950b6910d838870b33b73
BhojGC <gcbhoj@gmail.com>
2026-05-29 17:41:12 -0400

    - get tokenized story by id completed

--- python/controller/tokenized_data_learnsanskrit_controller.py
+++ python/controller/tokenized_data_learnsanskrit_controller.py
@@ -1,5 +1,9 @@
+from urllib import response
+
 from flask import jsonify, request
 from services.extract_new_fable_learnsanskrit import FetchNewFable
+from services.retrieve_all_tokenized_stories import RetrieveTokenizedStories
+from services.retrieve_tokenized_story_by_Id import RetrieveTokenizedStoryById
 
 
 ## Adding a new story to the collection
@@ -25,7 +29,44 @@ def add_new_story():
     
 ## Get All tokenized stories from collection
 
+def fetch_all_tokenized_stories():
+    
+    service = RetrieveTokenizedStories()
+    response = service.get_all()
+    
+    if response["success"]:
+        return jsonify(response),200
+    
+    return jsonify(response),500
+    
+
 ## Get tokenized story by id
 
+def fetch_tokenized_story_by_id():
+
+    # FETCH QUERY PARAM
+    story_id = request.args.get("story_id")
+
+    # VALIDATE INPUT
+    if not story_id:
+
+        return jsonify({
+            "success": False,
+            "message": "story_id query parameter is required"
+        }), 400
+
+    # CALL SERVICE
+    service = RetrieveTokenizedStoryById(story_id)
+
+    story = service.retrieve_story()
+
+    # SUCCESS RESPONSE
+    return jsonify({
+        "success": True,
+        "data": story
+    }), 200
+    
+
 ## Get tokenized story by category
+
 
