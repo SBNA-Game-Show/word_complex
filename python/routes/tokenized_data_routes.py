a2c84a442ddd1fdcc5d6293484c3d0437d7af68f
BhojGC <gcbhoj@gmail.com>
2026-06-01 13:45:40 -0400

    - bought in class from deployed repository

--- python/routes/tokenized_data_routes.py
+++ python/routes/tokenized_data_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flasgger import swag_from
 
 
-from controller.tokenized_data_learnsanskrit_controller import add_new_story,fetch_all_tokenized_stories,fetch_tokenized_story_by_id
+from controller.tokenized_data_learnsanskrit_controller import add_new_story,fetch_all_tokenized_stories,fetch_tokenized_story_by_id,fetch_tokenized_stories_by_category
 
 
 
@@ -23,3 +23,7 @@ tokenize_data_bp.route("/getAllTokenized",methods=["GET"])(get_all_tokenized_con
 ## Get tokenized story by Id
 get_tokenized_by_id_controller = swag_from("../swaggerdocs/tokenized_data/get_tokenized_story_by_id.yml")(fetch_tokenized_story_by_id)
 tokenize_data_bp.route("/getTokenizedById",methods=["GET"])(get_tokenized_by_id_controller)
+
+
+get_tokenized_stories_by_category_controller = swag_from("../swaggerdocs/tokenized_data/get_tokenized_stories_by_category.yml")(fetch_tokenized_stories_by_category)
+tokenize_data_bp.route("/getByCategory", methods=["GET"])(get_tokenized_stories_by_category_controller)
