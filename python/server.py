faa135e3e7876117fbbfcf2e1726154fcfb1ba13
BhojGC <gcbhoj@gmail.com>
2026-06-01 14:02:09 -0400

    - changes to server.py file to reflect changes

--- python/server.py
+++ python/server.py
@@ -1,48 +1,50 @@
 import json
 import os
+from flasgger import Swagger
 from flask import Flask,jsonify,request
 from flask_cors import CORS
-from config.envconfig import ENV
+from config.envconfig import PORT,DEBUG
+
 from config.dbconfig import connect_db
-
-from flask_swagger_ui import get_swaggerui_blueprint
-
-from bootstrap.bootstrap import AppBootstrap
 from middleware.error_handler import (register_error_handlers)
+from config.swagger_config import SWAGGER_CONFIG,SWAGGER_TEMPLATE
 
 from routes.tokenized_data_routes import tokenize_data_bp
 from routes.story_data_routes import story_data_bp
-
+from routes.user_routes import user_bp
 
 
 app = Flask(__name__)
 CORS(app)
 
+Swagger(app, config=SWAGGER_CONFIG, template=SWAGGER_TEMPLATE)
 
 db = connect_db()
 
-stories_collection = db.stories
-
-BASE_DIR = os.path.dirname(__file__)
-
-json_path = os.path.join(BASE_DIR, "data", "tokenized_stories.json")
+BASE_URL = "/api/v1/python"
 
 
-# Bootstrap run
-bootstrap = AppBootstrap(db, json_path)
-bootstrap.seed()
+@app.route("/")
+def home():
+    return jsonify({
+        "success": True,
+        "message": "SB Canada Dictionary API is running"
+    })
+    
+@app.route("/debug/files")
+def debug_files():
+    import os
 
-baseUrl = "/api/v1/python"
+    return {
+            "app_exists": os.path.exists("/app"),
+            "data_exists": os.path.exists("/app/data"),
+            "files": os.listdir("/app"),
+            "data_files": os.listdir("/app/data") if os.path.exists("/app/data") else []
+        }
 
-
-app.register_blueprint(story_data_bp,url_prefix = baseUrl)
-app.register_blueprint(tokenize_data_bp,url_prefix=baseUrl)
-
-
-
-
-
-
+app.register_blueprint(user_bp,url_prefix = BASE_URL)
+app.register_blueprint(story_data_bp,url_prefix = BASE_URL)
+app.register_blueprint(tokenize_data_bp,url_prefix=BASE_URL)
 
 
 
@@ -52,13 +54,10 @@ register_error_handlers(app)
 
 
 
-
-
-
 if __name__ == '__main__':
        app.run(
         host="0.0.0.0",
-        port=ENV["PORT"],
-        debug=ENV["NODE_ENV"] == "development"
+        port=PORT,
+        debug=DEBUG
     )
     
