e678ea1e64c69bd11c83527d93beac4422cd6a76
BhojGC <gcbhoj@gmail.com>
2026-06-01 20:33:22 -0400

    - dictionary route added to swagger

--- python/routes/dictionary_routes.py
+++ python/routes/dictionary_routes.py
@@ -10,4 +10,5 @@ dictionary_bp = Blueprint("dictionary_bp",__name__)
 
 # Get Route to tokenize english word with parts of speech and synonyms and antonyms
 
-dictionary_bp.route("/getEnglish",methods=["GET"])(tokenize_english_word)
+decorated_english_controller = swag_from("../swaggerdocs/dictionary/process_english_word.yml")(tokenize_english_word)
+dictionary_bp.route("/getEnglish",methods=["GET"])(decorated_english_controller)
