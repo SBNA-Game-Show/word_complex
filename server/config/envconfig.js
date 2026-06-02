fb2cfa20d9e5b74ee06c7bf383cc30014885c8bc
BhojGC <gcbhoj@gmail.com>
2026-06-02 15:16:25 -0400

    - removed python completely from the project - removed dev mongodb - configured express to connect only to atlas uri for both prod and development

--- server/config/envconfig.js
+++ server/config/envconfig.js
@@ -4,10 +4,7 @@ const ENV = {
   PORT: process.env.PORT || 5000,
   NODE_ENV: process.env.NODE_ENV || "development",
 
-  MONGO_URI:
-    process.env.NODE_ENV === "production"
-      ? process.env.ATLAS_URI
-      : process.env.MONGO_URI,
+  MONGO_URI: process.env.ATLAS_URI,
 };
 
 module.exports = ENV;
