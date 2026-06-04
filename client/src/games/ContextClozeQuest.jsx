dfd979db4624c1e6d8df9c65a3f81b858f4b0632
Jacob Makil <makiljacob@gmail.com>
2026-06-03 23:01:15 -0400

    Testing Backend/Frontend Connection 3

--- client/src/games/ContextClozeQuest.jsx
+++ client/src/games/ContextClozeQuest.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { getFillInBlanks } from "../services/api";
+
 export const meta = {
   id: "context-cloze-quest",
   title: "Context Cloze Quest",
@@ -6,10 +9,22 @@ export const meta = {
 };
 
 export default function ContextClozeQuest() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Context Cloze Quest</h1>
-      <p>My game is loading!</p>
-    </div>
-  );
+  const [gameData, setGameData] = useState(null);
+  useEffect(() => {
+    getFillInBlanks()
+      .then((response) => {
+        setGameData(response.data);
+      })
+      .catch(console.error);
+  }, []);
+  if (!gameData) {
+  return <p>Loading...</p>;
+}
+
+return (
+  <div style={{ padding: "20px" }}>
+    <h1>Context Cloze Quest</h1>
+    <p>{gameData.paragraph}</p>
+  </div>
+);
 }
